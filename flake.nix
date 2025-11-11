0c4d6ab9fba38b9e66c3afd127bd4148893b9dbf
Emory Merryman <emory.merryman@gmail.com>
2025-11-10 19:19:45 -0500

    resource

--- flake.nix
+++ flake.nix
@@ -61,14 +61,12 @@
                                                         {
                                                             extraBwrapArgs =
                                                                 [
-                                                                    "--bind /tmp/debug /debug"
                                                                     "--bind $MOUNT /mount"
                                                                     "--tmpfs /scratch"
                                                                 ] ;
                                                             name = "init-application" ;
                                                             runScript =
                                                                 ''
-                                                                    echo 1d3bd516 "$0 $#" >> /debug/DEBUG
                                                                     bash -c '
                                                                         if [[ -t 0 ]]
                                                                         then
