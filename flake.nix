fb1e53805dc08d9de36bf0b8cf0d2e44bf90421c
Emory Merryman <emory.merryman@gmail.com>
2025-11-10 19:41:29 -0500

    resource

--- flake.nix
+++ flake.nix
@@ -67,7 +67,6 @@
                                                             name = "init-application" ;
                                                             runScript =
                                                                 ''
-                                                                    echo 92629f1d "$0 $#" >> /tmp/DEBUG
                                                                     bash -c '
                                                                         if [[ -t 0 ]]
                                                                         then
