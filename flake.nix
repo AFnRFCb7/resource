cee6b001b1569d9620671fc84e00b8aa06dbc28d
Emory Merryman <emory.merryman@gmail.com>
2025-11-17 13:59:14 -0500

    write my own makeWrapper

--- flake.nix
+++ flake.nix
@@ -127,7 +127,7 @@
                                                                             pkgs.writeShellApplication
                                                                                 {
                                                                                     name = "makeWrapper" ;
-                                                                                    runtimeInputs = [ pkgs.coreutils ( _failure.implementation "8c4400a5" ) ] ;
+                                                                                    runtimeInputs = [ pkgs.coreutils ( failure "8c4400a5" ) ] ;
                                                                                     text =
                                                                                         ''
                                                                                             OUT="$1"
