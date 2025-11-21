f400457121ed7318d9ec12ab9dbcfeb4c37e8dd5
Emory Merryman <emory.merryman@gmail.com>
2025-11-21 10:02:22 -0500

    meaningless

--- flake.nix
+++ flake.nix
@@ -149,6 +149,7 @@
                                                                                     #
                                                                                     #
                                                                                     #
+                                                                                    #
                                                                                     name = "execute-init" ;
                                                                                     runtimeInputs = [ ] ;
                                                                                     text =
