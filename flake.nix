1064dedaa758175c7dbcc5476c9fb90c65cf1df4
Emory Merryman <emory.merryman@gmail.com>
2025-12-19 22:29:42 -0500

    WTF

--- flake.nix
+++ flake.nix
@@ -403,9 +403,9 @@
                                                                                     true
                                                                                     echo 6f25d941 >> /tmp/a1d983ad-DEBUG
                                                                                     true
-                                                                                    echo "${ findutils }/bin/find \"${ resources-directory }\" -exec stat {} \;" >> /tmp/a1d983ad-DEBUG
+                                                                                    echo "${ findutils }/bin/find \"${ resources-directory }/links\" -exec stat {} \;" >> /tmp/a1d983ad-DEBUG
                                                                                     true
-                                                                                    ${ findutils }/bin/find "${ resources-directory }" -exec stat {} \; >> /tmp/a1d983ad-DEBUG
+                                                                                    ${ findutils }/bin/find "${ resources-directory }/links" -exec stat {} \; >> /tmp/a1d983ad-DEBUG
                                                                                     true
                                                                                     echo 208b1eb4 >> /tmp/a1d983ad-DEBUG
                                                                                     mkdir --parents "${ resources-directory }/links/$INDEX"
