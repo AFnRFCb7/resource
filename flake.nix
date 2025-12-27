bcabf464c8c36209188f4902e71eed435ab1a639
Emory Merryman <emory.merryman@gmail.com>
2025-12-26 22:33:51 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -429,7 +429,7 @@
                                                                                             ''
                                                                                         else
                                                                                             ''
-                                                                                                echo b0adfa4b ${ input-application }
+                                                                                                echo b0adfa4b ${ init-application }
                                                                                                 if ${ init-application }/bin/init-application "${ arguments-nix }" > "$STANDARD_OUTPUT_FILE" 2> "$STANDARD_ERROR_FILE"
                                                                                                 then
                                                                                                     STATUS="$?"
