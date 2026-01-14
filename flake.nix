fafa5328cf64ea99a5c8b1800075d0eae87c6a38
Emory Merryman <emory.merryman@gmail.com>
2026-01-14 17:29:00 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -341,7 +341,7 @@
                                                                                 ARGUMENTS=( "$@" )
                                                                                 ARGUMENTS_JSON="$( printf '%s\n' "${ arguments-nix }" | jq -R . | jq -s . )"
                                                                                 TRANSIENT=${ transient }
-                                                                                ${ originator-pid-variable }="${ builtins.concatStringsSep "" [ "$" "{" originator-pid-variable ":=\"$( ps -o ppid= -p \\\"$PPID\\\" | tr -d '[:space:]')\" || failure 9db056a1"}" ] }"
+                                                                                ${ originator-pid-variable }="${ builtins.concatStringsSep "" [ "$" "{" originator-pid-variable ":=\"$( ps -o ppid= -p \\\"$PPID\\\" | tr -d '[:space:]')\" || failure 9db056a1" "}" ] }"
                                                                                 HASH="$( echo "${ pre-hash } ${ hash } $STANDARD_INPUT $HAS_STANDARD_INPUT" | sha512sum | cut --characters 1-128 )" || failure 2ea66adc
                                                                                 export HASH
                                                                                 mkdir --parents "${ resources-directory }/locks"
