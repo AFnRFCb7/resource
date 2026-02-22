85ae892b06e167623f509ac88bdda3f5c7f12a58
Emory Merryman <emory.merryman@gmail.com>
2026-02-22 14:20:30 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -503,14 +503,10 @@
                                                                     STANDARD_ERROR_FILE="$( mktemp )" || failure 56a44e28
                                                                     export STANDARD_ERROR_FILE
                                                                     STANDARD_OUTPUT_FILE="$( mktemp )" || failure a330cb07
-                                                                    echo 7e1212fd 14fcb221 >> /build/DEBUG
                                                                     export STANDARD_OUTPUT_FILE
-                                                                    echo 7e1212fd 98e26ed1 >> /build/DEBUG
                                                                     cd /
-                                                                    echo 7e1212fd bbb40432 >> /build/DEBUG
                                                                     if [[ "$HAS_STANDARD_INPUT" == "true" ]]
                                                                     then
-                                                                        echo 7e1212fd 9580b133 >> /build/DEBUG
                                                                         # shellcheck disable=SC2068
                                                                         if ${ applications.init } ${ builtins.concatStringsSep "" [ "$" "{" "ARGUMENTS[@]" "}" ] } < "$STANDARD_INPUT_FILE" > "$STANDARD_OUTPUT_FILE" 2> "$STANDARD_ERROR_FILE"
                                                                         then
