636c6dd8d175b8b9a4cb93fc9e7a56cf078302e4
Emory Merryman <emory.merryman@gmail.com>
2026-02-26 06:38:59 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -516,6 +516,7 @@
                                                                     PENULTIMATE_PID="$( ps -o ppid= -p "$PPID" | tr -d '[:space:]' )" || failure d79214f2
                                                                     ULTIMATE_PID="$( ps -o ppid= -p "$PENULTIMATE_PID" | tr -d '[:space:]' )" || failure e1556ee8
                                                                 fi
+                                                                export ULTIMATE_PID
                                                                 # originator-pid-variable ${ builtins.toString depth } "$ULTIMATE_PID"
                                                                 mkdir --parents ${ resources-directory }
                                                                 ARGUMENTS=( "$@" )
