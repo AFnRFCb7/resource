9ced44a40948b7056ed3a3b60048c32391958670
Emory Merryman <emory.merryman@gmail.com>
2026-02-26 06:03:03 -0500

    disable

--- flake.nix
+++ flake.nix
@@ -506,6 +506,7 @@
                                                                             runtimeInputs = [ coreutils jq redis failure ] ;
                                                                             text =
                                                                                 ''
+                                                                                    # shellcheck disable=SC2089,SC2016,SC2090
                                                                                     DESCRIPTION='${ builtins.toJSON ( description secondary ) }'
                                                                                     export DESCRIPTION
                                                                                     JSON="$( cat | jq --compact-output '.' )" || failure 64cec474
