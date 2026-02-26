d1b009dd87a66dfed7cceeb2e49045a95c79828f
Emory Merryman <emory.merryman@gmail.com>
2026-02-26 07:34:09 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -471,7 +471,7 @@
                                                                                 ''
                                                                                     DEPTH="$1"
                                                                                     PID="$2"
-                                                                                    # echo mkdir --parents "${ resources-directory }/$INDEX/originator-pids" > /build/DEBUG
+                                                                                    # echo mkdir --parents "${ resources-directory }/$INDEX/originator-pids"
                                                                                     # touch "${ resources-directory }/$INDEX/originator-pids/$PID"
                                                                                     export DEPTH
                                                                                     export PID
